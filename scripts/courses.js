ca0dc3a02dec87d3abb7fc5bc0eccbb790f2467b
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-08-28 22:52:12 +0500

    [+] added course detail

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,11 +1,16 @@
 const courses = [
-  {title: "Office Automation"},
-  {title: "CIT"},
-  {title: "DIT"},
-  {title: "Search Engine Optimization (SEO)"},
-  {title: "Introduction to Web Development"},
-  {title: "Front-End Development"},
-  {title: "Back-End Development"},
-  {title: "Wordpress Development"},
-  {title: "Graphic Designing"},
-]
+  {
+    title: "Office Automation",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, ex.",
+    skills: [],
+    duration: 10,
+  },
+  { title: "CIT" },
+  { title: "DIT" },
+  { title: "Search Engine Optimization (SEO)" },
+  { title: "Introduction to Web Development" },
+  { title: "Front-End Development" },
+  { title: "Back-End Development" },
+  { title: "Wordpress Development" },
+  { title: "Graphic Designing" },
+];
