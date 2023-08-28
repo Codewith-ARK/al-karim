6f7215ed2bc1d84fa95dde5ac4b789ff4fa9ef9d
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-08-28 20:14:24 +0500

    ✔ completed landing page

---
+++ scripts/courses.js
@@ -0,0 +1,11 @@
+const courses = [
+  {title: "Office Automation"},
+  {title: "CIT"},
+  {title: "DIT"},
+  {title: "Search Engine Optimization (SEO)"},
+  {title: "Introduction to Web Development"},
+  {title: "Front-End Development"},
+  {title: "Back-End Development"},
+  {title: "Wordpress Development"},
+  {title: "Graphic Designing"},
+]
