719c49db5bb2f9272665a54baa26c5fa6da94e98
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-08-31 17:35:32 +0500

    [🧹] cleaned the code

---
+++ scripts/courseCards.js
@@ -0,0 +1,29 @@
+function renderCourseCards() {
+  courseData.map((course) => {
+    createCourseCard(course);
+  });
+}
+
+function createCourseCard(obj) {
+  const truncatedText = obj.desc.length > 95 ? obj.desc.substring(0, 95 - 3) + "..." : obj.desc;
+
+  let html = `
+  <a href="course_details/course-detail.html?id=${obj.id}">
+    <div class="card">
+    <img class="card-img-top" src="${obj.imgUrl}" alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">${obj.title}</h5>
+      <p class="card-text">
+        ${truncatedText}
+      </p>
+    </div>
+    </div>
+  </a>
+  `;
+
+  if (document.querySelector("#all-courses")) {
+    document.querySelector("#all-courses").innerHTML += html;
+  }
+}
+
+window.onload = renderCourseCards;
